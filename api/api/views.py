bf05ceca52af281ce4e211fde239044598428a5d
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2019-05-26 01:24:02 +0200

    Add user/id get post put and delete verbs

--- api/api/views.py
+++ api/api/views.py
@@ -1,5 +1,5 @@
 from django.utils.decorators import method_decorator
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.core.exceptions import ObjectDoesNotExist
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
@@ -28,56 +28,65 @@ class CORSResponse(JsonResponse):
         self['Access-Control-Allow-Origin'] = '*'
 
 
+class APIResponse(CORSResponse):
+
+    def __init__(self, code, reason, data={}, *args, **kwargs):
+        super().__init__({
+            'statuscode': code,
+            'reason': reason,
+            'data': data
+        }, *args, **kwargs)
+
+
 @method_decorator(csrf_exempt, name='dispatch')
 class APIView(View):
     model = None
 
     def get(self, request, *args, **kwargs):
         try:
-            object_ = get_object_or_404(self.model, id=kwargs['id'])
-            return JsonResponse({
-                'statuscode': 200,
-                'reason': f"{object_.Meta.verbose_name} retrieved successfully",
-                'data': object_.json()
-                })
-        except Http404:
-            return JsonResponse({
-                'statuscode': 404,
-                'reason': f"{self.model.Meta.verbose_name} not found",
-                'data': {}
-                })
+            object_ = self.model.objects.get(id=kwargs['id'])
+            return APIResponse(200, f"{self.model.Meta.verbose_name} retrieved successfully", object_.json())
+        except ObjectDoesNotExist:
+            return APIResponse(404, f"{self.model.Meta.verbose_name} not found")
         except Exception as e:
-            return JsonResponse({
-                'statuscode': 500,
-                'reason': str(e),
-                'data': {}
-                })
+            return JsonResponse(500, str(e))
 
     def post(self, request, *args, **kwargs):
         data = request.body.decode('utf-8')
         json_data = json.loads(data)
         try:
             object_ = self.model.objects.create(**json_data)
-            return JsonResponse({
-                'statuscode': 200,
-                'reason': f"{object_.Meta.verbose_name} created successfully",
-                'data': object_.json()
-                })
+            return APIResponse(201, f"{self.model.Meta.verbose_name} created successfully", object_.json())
         except Exception as e:
-            return JsonResponse({
-                'statuscode': 500,
-                'reason': str(e),
-                'data': {}
-                })
+            return APIResponse(500, str(e))
 
     def put(self, request, *args, **kwargs):
-        pass
+        data = request.body.decode('utf-8')
+        json_data = json.loads(data)
+        try:
+            object_ = self.model.objects.get(id=kwargs['id'])
+            json_data.pop('id', None)
+            for key, value in json_data.items():
+                setattr(object_, key, value)
+            object_.save()
+            return APIResponse(200, f"{self.model.Meta.verbose_name} updated successfully", object_.json())
+        except ObjectDoesNotExist:
+            object_ = self.model(**json_data)
+            object_.id = kwargs['id']
+            object_.save()
+            return APIResponse(200, f"{self.model.Meta.verbose_name} created successfully", object_.json())
+
 
     def patch(self, request, *args, **kwargs):
         pass
 
     def delete(self, request, *args, **kwargs):
-        pass
+        try:
+            object_ = self.model.objects.get(id=kwargs['id'])
+            object_.delete()
+            return APIResponse(200, f"{self.model.Meta.verbose_name} deleted successfully", object_.json())
+        except ObjectDoesNotExist:
+            return APIResponse(404, f"{self.model.Meta.verbose_name} not found")
 
 
 class UserView(APIView):
