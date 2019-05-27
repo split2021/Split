11a7185180ffe84bd2a5d913aa955e25e3b74e63
VM-Split <split_2021@labeip.epitech.eu>
2019-05-27 08:22:03 +0000

    Convert DEBUG to bool

--- api/split/settings.py
+++ api/split/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ni^)v*b622$&8@8mf)l!&0()iotz4s%#o-f$q_g6v719p6n_3e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = bool(os.environ['DEBUG'])
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '52.178.136.18']
 
