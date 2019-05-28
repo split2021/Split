f39c2dc5861bff186ad8cce618d5263e147bd7fd
VM-Split <split_2021@labeip.epitech.eu>
2019-05-28 14:16:30 +0000

    Add Metting model

--- api/eip/models.py
+++ api/eip/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.conf import settings
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
 def get_pld_path(instance, filename):
-    return f"eip/pld/%Y/%m/%d/{}"
-
+        return f"eip/pld/%Y/%m/%d/{filename}"
 
 class ProjectLogDocument(models.Model):
     """
@@ -17,4 +18,18 @@ class ProjectLogDocument(models.Model):
     ]
 
     type = models.CharField(max_length=2, choices=PLD_TYPE_CHOICES)
-    file = models.FielField(upload_to=)
+    file = models.FielField(upload_to=get_pld_path)
+    meeting = models.ForeignKey('Meeting', on_delete=models.SET_NULL, null=True)
+
+    def clean(self):
+        if not file.filename.endswith(".pdf"):
+            raise ValidationError("The file must be of pdf type")
+
+
+class Meeting(models.Model):
+    """
+    """
+
+    name = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL)
