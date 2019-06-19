5f01074197b40739d657b851788ac542296fb405
Tiphaine LAURENT <tlaurent@ixemelis.fr>
2019-06-19 15:51:58 +0200

    Create a start script to change Django execution
    Test for DEBUG environment variable and :
    - Execute gunicorn for production
    - Execute runserver for development
    This let the use of static files in development
    
    Replace CRLF by LF

--- api/start.sh
+++ api/start.sh
@@ -3,5 +3,5 @@
 if [[ "$DEBUG" = "0" ]]; then
   gunicorn split.wsgi:application --bind 0.0.0.0:8000 --workers 3
 elif [[ "$DEBUG" = "1" ]]; then
-  python manage.py runserver
+  python manage.py runserver 0.0.0.0:8000
 fi
