c2dee8bcd4cdcfed1ebbc29460f94727351fbb3d
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2019-06-22 18:37:57 +0200

    Filter user list with only EIP members

--- api/eip/admin.py
+++ api/eip/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from api.models import User
 from eip.models import ProjectLogDocument, Meeting
 
 # Register your models here.
@@ -8,10 +9,15 @@ from eip.models import ProjectLogDocument, Meeting
 class ProjectLogDocumentAdmin(admin.ModelAdmin):
     """
     """
-    list_display = ('type', 'file', 'meeting')    
+    list_display = ('type', 'file', 'meeting')
 
 @admin.register(Meeting)
 class MeetingAdmin(admin.ModelAdmin):
     """
     """
     filter_horizontal = ('members',)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "members":
+            kwargs["queryset"] = User.objects.filter(groups__name="EIP")
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
