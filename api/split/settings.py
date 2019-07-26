6a7c7f4299827a04e72f03cf785574118024590a
Tiphaine LAURENT <tlaurent@ixemelis.fr>
2019-07-26 15:59:02 +0200

    Add an info log file

--- api/split/settings.py
+++ api/split/settings.py
@@ -31,6 +31,11 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
         'console': {
             'class': 'logging.StreamHandler',
         },
