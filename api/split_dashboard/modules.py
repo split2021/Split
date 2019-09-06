1e7609d78a0bf61a5b00f271b19b590faa073e6f
Tiphaine LAURENT <tlaurent@ixemelis.fr>
2019-09-06 16:47:09 +0200

    Split long strings

--- api/split_dashboard/modules.py
+++ api/split_dashboard/modules.py
@@ -8,7 +8,9 @@ from api.models import Log
 
 class Calendar(DashboardModule):
     title = "Split calendar"
-    title_url = "https://calendar.google.com/calendar/embed?src=0q3rugurp4nqr8s2e2tlsqu9jc%40group.calendar.google.com&ctz=Europe%2FBrussels"
+    title_url = ("https://calendar.google.com/calendar/embed"
+                "?src=0q3rugurp4nqr8s2e2tlsqu9jc%40group.calendar.google.com"
+                "&ctz=Europe%2FBrussels")
     template = 'split_dashboard/modules/calendar.html'
 
 
@@ -29,7 +31,10 @@ class Documentation(LinkList):
             },
             {
                 'title': _("API"),
-                'url': "https://split2021.postman.co/collections/6975668-bede7db7-aa15-4f0a-afd2-ff562c6a2e86?version=latest&workspace=4a5d98f4-d78c-46c2-8285-56f173abf468",
+                'url': ("https://split2021.postman.co/collections/"
+                       "6975668-bede7db7-aa15-4f0a-afd2-ff562c6a2e86"
+                       "?version=latest"
+                       "&workspace=4a5d98f4-d78c-46c2-8285-56f173abf468")
                 'external': True,
             },
         ]
