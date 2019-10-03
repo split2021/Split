8f337ff2edd87344ec18d239591115991ff09876
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2019-10-03 15:25:26 +0200

    Add GroupMembership api path

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 
-from api.views import LoginView, UserView, UsersView, GroupView, GroupsView
+from api.views import LoginView, UserView, UsersView, GroupMembershipView, GroupView, GroupsView
 
 
 urlpatterns = [
     path('login', LoginView.as_view()),
     path('users/<int:id>', UserView.as_view()),
     path('users/', UsersView.as_view()),
+    path('group_memberships/<int:id>', GroupMembershipView.as_view()),
     path('groups/<int:id>', GroupView.as_view()),
     path('groups/', GroupsView.as_view()),
 ]
