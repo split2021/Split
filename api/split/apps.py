1a7d91b810791ce73e87024133d1481e78310b4c
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2019-10-12 14:52:19 +0200

    Remove hard api

--- api/split/apps.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.admin.apps import AdminConfig
-
-
-class SplitAdminConfig(AdminConfig):
-    default_site = 'split.admin.SplitAdminSite'
--- api/split_dashboard/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from api.models import User
-
-
-class UserUpdateForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'email', 'username', 'phone']
