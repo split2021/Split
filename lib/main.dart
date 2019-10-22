88d4cd69850e71b4b6655dea167858c9d2068451
Thibault Lecointe <thibault.lecointe@epitech.eu>
2019-10-22 03:48:41 +0200

    basics test for flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title:'HomePage',
       theme: ThemeData(
         primaryColor: Color.fromRGBO(85, 112, 221, 1.0),
       ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,37 +7,61 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:split/login/login.dart';
+import 'package:split/home/home.dart';
+import 'package:split/contact/contact_page.dart';
+import 'package:split/settings/settings_page.dart';
+
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
+
 
 void main() {
-/*
-testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+
+testWidgets('Check if every elements in home page are here + Click On Connect', (WidgetTester tester) async {
     // Create the widget by telling the tester to build it.
-    await tester.pumpWidget(LogIn());
+    await tester.pumpWidget(MaterialApp(
+    home: Scaffold(
+      body: LogIn(),
+    ),
+  ));
+    expect(find.text('Mot de passe oublié ?'), findsOneWidget);
+    expect(find.text('Nouveau ? Créer un compte'), findsOneWidget);
+    expect(find.text('Connexion'), findsOneWidget);
+    expect(find.text('Email'), findsOneWidget);
+    expect(find.text('Password'), findsOneWidget);
+    await tester.tap(find.byType(RaisedButton));
+    await tester.pumpAndSettle();
   });
-test('Check connexion', () async {
-   
-    //await tester.pumpWidget(LogIn());
-    //await tester.enterText(find.byType(TextField), 'hi');
+testWidgets('Navigation to Page Home', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(MaterialApp(
+    home: Scaffold(
+      body: Home(),
+    ),
+  ));
+   expect(find.byType(AppBar), findsOneWidget);
+   expect(find.byType(PageView), findsOneWidget);
   });
-*/
-  /*
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Contacts', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(MaterialApp(
+    home: Scaffold(
+      body: ContactPage(),
+    ),
+  ));
+   expect(find.byType(Container), findsOneWidget);
   });
-  */
+
+  testWidgets('SettingsPage', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(MaterialApp(
+    home: Scaffold(
+      body: SettingsPage(),
+    ),
+  ));
+  ////
+  //Wait for the Page
+  ////
+  });
 }
