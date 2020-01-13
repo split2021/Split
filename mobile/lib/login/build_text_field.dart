32e37a9355fdcb9555cb1d53646bb26ead335cf9
Hugo Martin <hugo1.martin@epitech.eu>
2020-01-13 02:08:38 +0100

    Login / create account / get contact / requests

--- mobile/lib/login/build_text_field.dart
+++ mobile/lib/login/build_text_field.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
-Widget buildTextFieldLogIn(String hintText, BuildContext context,
-    var controller, bool obscureText, Function showHidePassword) {
+import '../user/user_class.dart';
+
+Widget buildTextFieldLogIn(
+  String hintText,
+  BuildContext context,
+  var controller,
+  bool obscureText,
+  Function showHidePassword,
+) {
   return Container(
     height: 40,
     padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
     child: TextField(
+      onChanged: (value) {
+        if (hintText == 'Email')
+          User.username = value;
+        else
+          User.password = value;
+      },
       decoration: InputDecoration(
           hintText: hintText,
           hintStyle: TextStyle(color: Colors.grey, fontSize: 15, height: 0.50),
