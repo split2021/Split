44fbf2d95f085d4df90ce1589b6d60deb260dbfa
Valentin Rubio <valentin.rubio@epitech.eu>
2020-04-16 00:17:23 -0700

    New profile page design

--- mobile/lib/profile/profile_page.dart
+++ mobile/lib/profile/profile_page.dart
@@ -35,87 +35,257 @@ class _ProfilePageState extends State<ProfilePage> {
     User.username = null;
   }
 
+  Widget buildBackground(Size screenSize) {
+    return Container(
+      height: screenSize.height / 2.6,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/profile_bg.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget buildProfilePicture() {
+    return Center(
+      child: Container(
+        width: 140.0,
+        height: 140.0,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/default_profile.jpg'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.circular(80.0),
+          border: Border.all(
+            color: Colors.white,
+            width: 10.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildUsername() {
+    TextStyle _nameTextStyle = TextStyle(
+      fontFamily: 'Roboto',
+      color: Colors.black,
+      fontSize: 28.0,
+      fontWeight: FontWeight.w700,
+    );
+
+    return Text(
+      this.userFullname,
+      style: _nameTextStyle,
+    );
+  }
+
+  Widget buildTitle(BuildContext context, String title) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 6.0),
+      decoration: BoxDecoration(
+        color: Theme.of(context).scaffoldBackgroundColor,
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'Spectral',
+          color: Colors.black,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    );
+  }
+
+  Widget buildStats(String friends, String groups, String score) {
+    return Container(
+      height: 60.0,
+      margin: EdgeInsets.only(top: 8.0),
+      decoration: BoxDecoration(
+        color: Color(0xFFEFF4F7),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          buildStat("Amis", friends),
+          buildStat("Groupes", groups),
+          buildStat("Score", score),
+        ],
+      ),
+    );
+  }
+
+  Widget buildStat(String label, String count) {
+    TextStyle _statLabelTextStyle = TextStyle(
+      fontFamily: 'Roboto',
+      color: Colors.black,
+      fontSize: 16.0,
+      fontWeight: FontWeight.w300,
+    );
+
+    TextStyle _statCountTextStyle = TextStyle(
+      color: Colors.black54,
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+    );
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          count,
+          style: _statCountTextStyle,
+        ),
+        Text(
+          label,
+          style: _statLabelTextStyle,
+        ),
+      ],
+    );
+  }
+
+  Widget buildSeparator(Size screenSize) {
+    return Container(
+      width: screenSize.width / 1.6,
+      height: 2.0,
+      color: Colors.black54,
+      margin: EdgeInsets.only(top: 4.0),
+    );
+  }
+
+  Widget buildButtons(String labelOne, MaterialPageRoute routeOne,
+      String labelTwo, MaterialPageRoute routeTwo) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                Navigator.push(context, routeOne);
+              },
+              child: Container(
+                height: 40.0,
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  color: Color(0xFF404A5C),
+                ),
+                child: Center(
+                  child: Text(
+                    labelOne,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: 10.0),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                Navigator.push(context, routeTwo);
+              },
+              child: Container(
+                height: 40.0,
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      labelTwo,
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildLogOutButton(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                logOut(context);
+              },
+              child: Container(
+                height: 40.0,
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  color: Color(0xFF404A5C),
+                ),
+                child: Center(
+                  child: Text(
+                    "Disconnect",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
     if (this.userFullname == null)
       return new Container();
     else
-      return Container(
-        child: new Stack(
+      return Scaffold(
+        body: Stack(
           children: <Widget>[
-            Center(
-                child: Column(children: <Widget>[
-              Align(
-                  alignment: FractionalOffset.topCenter,
-                  child: Padding(
-                      padding: EdgeInsets.only(top: 10.0, bottom: 50.0),
-                      child: Text("Welcome, " + this.userFullname,
-                          style: TextStyle(
-                              color: Color.fromRGBO(71, 50, 128, 1.0),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24)))),
-              new GestureDetector(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => EditProfile()));
-                  },
-                  child: new Container(
-                      height: 40,
-                      margin: const EdgeInsets.all(5.0),
-                      width: MediaQuery.of(context).size.width - 60,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(40.0),
-                          gradient: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              colors: <Color>[
-                                Color.fromRGBO(85, 112, 221, 1.0),
-                                Color.fromRGBO(71, 50, 128, 1.0),
-                              ])),
-                      child: Center(
-                          child: Text(
-                        "Edit profile",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      )))),
-              new GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
+            buildBackground(screenSize),
+            SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: screenSize.height / 5),
+                    buildProfilePicture(),
+                    buildUsername(),
+                    buildTitle(context, "Splitter aguerri"),
+                    buildStats("14", "2", "79"),
+                    // _buildBio(context),
+                    SizedBox(height: 10.0),
+                    buildSeparator(screenSize),
+                    SizedBox(height: 8.0),
+                    buildButtons(
+                        "Add friend",
+                        MaterialPageRoute(builder: (context) => EditProfile()),
+                        "Invite in group",
                         MaterialPageRoute(
-                            builder: (context) => PaymentMethods()));
-                  },
-                  child: new Container(
-                      height: 40,
-                      margin: const EdgeInsets.all(5.0),
-                      width: MediaQuery.of(context).size.width - 60,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(40.0),
-                          gradient: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              colors: <Color>[
-                                Color.fromRGBO(85, 112, 221, 1.0),
-                                Color.fromRGBO(71, 50, 128, 1.0),
-                              ])),
-                      child: Center(
-                          child: Text(
-                        "View payment methods",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
-                      )))),
-            ])),
-            new Positioned(
-                child: new Align(
-                    alignment: FractionalOffset.bottomRight,
-                    child: Padding(
-                        padding: EdgeInsets.only(bottom: 10.0, right: 30.0),
-                        child: FloatingActionButton(
-                            onPressed: () {
-                              logOut(context);
-                            },
-                            tooltip: 'Disconnect',
-                            backgroundColor: Color.fromRGBO(71, 50, 128, 1.0),
-                            splashColor: Colors.white,
-                            child: Icon(Icons.exit_to_app)))))
+                            builder: (context) => PaymentMethods())),
+                    buildButtons(
+                        "Edit personal infos",
+                        MaterialPageRoute(builder: (context) => EditProfile()),
+                        "Edit title",
+                        MaterialPageRoute(
+                            builder: (context) => PaymentMethods())),
+                    buildLogOutButton(context),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       );
