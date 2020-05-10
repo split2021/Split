192d5df0a327dab6d5c3209467dcd68f22edf28e
Nicolas <nicolas.djurovic@epitech.eu>
2020-05-10 18:40:40 +0200

    route api change

--- web/website/src/components/Api/Api.jsx
+++ web/website/src/components/Api/Api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: `http://127.0.0.1:8080/api/`,
+    baseURL: `http://40.112.78.121:80/api/`,
+    //baseURL: `http://127.0.0.1:8080/api/`,
     //baseURL: `http://52.178.136.18:443/api/`,
     responseType: "json", 
 });
