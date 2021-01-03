159148c601b1901107009d4059c512cd5fd92085
Matthieu Lachenait <lachenaitmat@gmail.com>
2021-01-03 15:40:22 +0100

    [WEB] [FIX] Remove log

--- web/website/src/components/MobilePages/Result.js
+++ web/website/src/components/MobilePages/Result.js
@@ -10,7 +10,6 @@ export default class Result extends React.Component {
   render() {
     let url = this.props.location.search;
     let params = queryString.parse(url);
-    console.log(params);
     return (
       <Container>
         <Title>{params.title || ''}</Title>
